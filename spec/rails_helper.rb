b723e4849819058d1832af41694d1722f0cdbcdd
Gionani <montana_malboro_montana@hotmail.com>
2015-12-09 18:07:44 -0800

    final prueba

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,7 +29,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.include Devise::TestHelpers
+  config.include Devise::TestHelpers, :type => :controlers 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
@@ -56,6 +56,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
 config.include Warden::Test::Helpers
 
 config.after do
